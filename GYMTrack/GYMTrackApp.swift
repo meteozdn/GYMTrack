df9c4d45fcac4d44cd16468291d02273db60e0db
Metehan Özden <metehanozden.bil@gmail.com>
2024-11-16 23:59:06 +0300

    Initial Commit

---
+++ GYMTrack/GYMTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  GYMTrackApp.swift
+//  GYMTrack
+//
+//  Created by Metehan Özden on 16.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct GYMTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
